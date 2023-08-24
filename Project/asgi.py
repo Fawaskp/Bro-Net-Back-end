a631df6b3333541bb05e5f92179a59601f662644
Fawas <fawaskp1010@gmail.com>
2023-08-24 23:09:29 +0530

    django setup done in asgi.py

--- Project/asgi.py
+++ Project/asgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project.settings")
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
